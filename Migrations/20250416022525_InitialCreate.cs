1b68d7f3108ba65f26f4925f54383416d3605cdb
Matheus Felipe Silvério da Silveira <matheusf.silveira@hotmail.com>
2025-04-17 12:53:23 -0300

    add migrations for Loja and Product models

---
+++ Migrations/20250416022525_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ProductControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lojas",
+                columns: table => new
+                {
+                    IdLoja = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lojas", x => x.IdLoja);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    IdProduct = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Produto = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    DataEntrega = table.Column<DateOnly>(type: "date", nullable: false),
+                    Validade = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FinalizadoEm = table.Column<DateOnly>(type: "date", nullable: true),
+                    IdLoja = table.Column<int>(type: "integer", nullable: false),
+                    LojaIdLoja = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.IdProduct);
+                    table.ForeignKey(
+                        name: "FK_Products_Lojas_LojaIdLoja",
+                        column: x => x.LojaIdLoja,
+                        principalTable: "Lojas",
+                        principalColumn: "IdLoja",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_LojaIdLoja",
+                table: "Products",
+                column: "LojaIdLoja");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Lojas");
+        }
+    }
+}
---
+++ Migrations/20250416155737_FixProductLojaRelation.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProductLojaRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Lojas_LojaIdLoja",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_LojaIdLoja",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LojaIdLoja",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_IdLoja",
+                table: "Products",
+                column: "IdLoja");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Lojas_IdLoja",
+                table: "Products",
+                column: "IdLoja",
+                principalTable: "Lojas",
+                principalColumn: "IdLoja",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Lojas_IdLoja",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_IdLoja",
+                table: "Products");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LojaIdLoja",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_LojaIdLoja",
+                table: "Products",
+                column: "LojaIdLoja");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Lojas_LojaIdLoja",
+                table: "Products",
+                column: "LojaIdLoja",
+                principalTable: "Lojas",
+                principalColumn: "IdLoja",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
