e0552640732c0d1fe407954610bf7a38dfa294e7
Matheus Felipe Silvério da Silveira <matheusf.silveira@hotmail.com>
2025-04-17 12:56:07 -0300

    add temporary build files (not for production)

--- obj/Debug/net8.0/ProductControl.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductControl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cccae476afbd08f94edd25579b5d82505b0c50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1563fedbd6c550e14b14a237b374e1ca67b1ac0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
