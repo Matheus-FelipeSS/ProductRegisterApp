1103fc0ffa0e192f2c6d7cce3e0309ac7662022f
Matheus Felipe Silvério da Silveira <matheusf.silveira@hotmail.com>
2025-04-22 23:17:20 -0300

    feat: Update hamburger menu for mobile version
    - Add responsive hamburger menu icon
    - Implement toggle functionality (open/close)
    - Enhance mobile experience with smooth transitions
    - Adjust menu item spacing and sizing

--- obj/Debug/net8.0/ProductControl.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductControl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3417abe2e2e711dd8ea23b10eb4ae828f30b161f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72b687b1f5b80e31cc32e54e5b6cd26dfef0592")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
